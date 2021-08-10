199052d3d4c2eb3ce2de04caadc0a38dd0aa549d
Shourya <shouryab2001@gmail.com>
2021-08-10 22:06:49 +0530

    twelfth commit : some correction

--- assets/js/home_task.js
+++ assets/js/home_task.js
@@ -20,13 +20,13 @@
     }
 
     let createNewTaskDom = function(task){
-        return $(`<li id="post-${ i._id }">
+        return $(`<li id="task-${ i._id }">
                         <form action="/task-complete" method="POST">
                             <input type="checkbox" name="checkbox" onChange="this.form.submit()" value="${ i._id }" id="check">
                             <label class="container">
                                 ${ i.task }
                                 <small>
-                                    <a href="/task-delete/${ i.id }" class="delete delete-post-button"><i class="fas fa-times-circle"></i></a>
+                                    <a href="/task-delete/${ i.id }" class="delete delete-task-button"><i class="fas fa-times-circle"></i></a>
                                 </small> 
                                 <small>
                                     <p>Last Date - ${ i.lastDate }</p>
